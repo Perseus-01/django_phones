bde505ea32f60b5506dddb83a78ed653be483c17
Alexey Chizhevskiy <aleksei.chizhevskii@gmail.com>
2023-06-22 18:58:29 +0500

    added orm homework

---
+++ phones/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from phones.models import Phone
+
+
+def index(request):
+    return redirect('catalog')
+
+
+def show_catalog(request):
+    template = 'catalog.html'
+    
+    sorting = request.GET.get('sort')
+    if sorting == 'name':
+        phones = Phone.objects.order_by(sorting)
+    elif sorting =='min_price':
+        phones = Phone.objects.order_by('-price')
+    elif sorting =='max_price':
+        phones = Phone.objects.order_by('price')
+    else:
+        phones = Phone.objects.all()
+    
+    context = {'phones': phones}
+    return render(request, template, context)
+
+
+def show_product(request, slug):
+    template = 'product.html'
+    phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone
+    }
+    return render(request, template, context)
